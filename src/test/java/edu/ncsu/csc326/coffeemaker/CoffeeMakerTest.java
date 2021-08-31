67323fba1097643e88f27463072bf69f1cfbc5c8
noboomta <puvana.jp36@gmail.com>
2021-08-31 20:40:47 +0700

    done writing test

--- src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
+++ src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
@@ -119,11 +119,26 @@ public class CoffeeMakerTest {
 		coffeeMaker.addInventory("4", "-1", "str", "3");
 	}
 
+	/**
+	 * Given a coffee maker with the default inventory
+	 * When we add inventory with the formal quantities
+	 *
+	 * @throws InventoryException  if there was an error parsing the quanity
+	 * 		to a positive integer.
+	 */
 	@Test
 	public void testAddNormalInventory() throws InventoryException {
 		coffeeMaker.addInventory("2", "3", "4", "5");
 	}
 
+	/**
+	 * Given a coffee maker with the default inventory
+	 * When we add inventory with formal quantities
+	 * The string from checkInventory() has to be the same as expected.
+	 *
+	 * @throws InventoryException  if there was an error parsing the quanity
+	 * 		to a positive integer.
+	 */
 	@Test
 	public void testCheckInventory() throws InventoryException {
 		coffeeMaker.addInventory("0","1","2","3");
@@ -131,19 +146,32 @@ public class CoffeeMakerTest {
 		assertEquals(expectedInventory, coffeeMaker.checkInventory());
 	}
 
+	/**
+	 * Given a coffee maker with the default inventory
+	 * When we add recipe to recipe book and call the
+	 * makeCoffee with the enough money the chance should be correct.
+	 */
 	@Test
 	public void testCheckChance() {
 		coffeeMaker.addRecipe(recipe1);
 		assertEquals(5, coffeeMaker.makeCoffee(0, 55));
 	}
 
+	/**
+	 * Given a coffee maker with the default inventory
+	 * But we Don't add recipe to recipe book so after call the
+	 * makeCoffee with the money the chance should not be changed.
+	 */
 	@Test
 	public void testCheckChanceOfNullRecipe() {
 		assertEquals(50, coffeeMaker.makeCoffee(0, 50));
 	}
 
 	/**
-	 *
+	 * Given a coffee maker with the default inventory
+	 * When we add recipe to recipe book and call the
+	 * makeCoffee with the enough money the inventory
+	 * should be decreased as it should be.
 	 */
 	@Test
 	public void testInventoryReducedCorrectly() {
@@ -156,7 +184,7 @@ public class CoffeeMakerTest {
 	/**
 	 * Given a coffee maker with one valid recipe
 	 * When we make coffee, selecting the valid recipe and paying more than
-	 * 		the coffee costs
+	 * 	the coffee costs
 	 * Then we get the correct change back.
 	 */
 	@Test
@@ -165,6 +193,22 @@ public class CoffeeMakerTest {
 		assertEquals(25, coffeeMaker.makeCoffee(0, 75));
 	}
 
+	/**
+	 * Given a coffee maker with one valid recipe
+	 * When we make coffee, selecting the valid recipe and paying more than
+	 * the coffee costs
+	 * Then we get change that equal to the input back.
+	 */
+	@Test
+	public void testMoneyNotEnough() {
+		coffeeMaker.addRecipe(recipe1);
+		assertEquals(25, coffeeMaker.makeCoffee(0, 25));
+	}
+
+	/**
+	 * Given a coffee maker with 4 valid recipe
+	 * The coffee maker should not able to add it, since they can add just 3.
+	 */
 	@Test
 	public void testOverRecipes() {
 		coffeeMaker.addRecipe(recipe1);
@@ -174,6 +218,10 @@ public class CoffeeMakerTest {
 		assertEquals(3, coffeeMaker.getRecipes().length);
 	}
 
+	/**
+	 * Given a coffee maker with 4 valid recipe
+	 * The coffee maker should not able to add it, since they can add just 3.
+	 */
 	@Test
 	public void testAddExceedAmountOfRecipe(){
 		coffeeMaker.addRecipe(recipe1);
@@ -182,6 +230,9 @@ public class CoffeeMakerTest {
 		assertFalse(coffeeMaker.addRecipe(recipe4));
 	}
 
+	/**
+	 * Given a coffee maker 2 of same recipe it should not able to make it.
+	 */
 	@Test
 	public void testAddDuplicateRecipes() {
 		coffeeMaker.addRecipe(recipe1);
@@ -189,16 +240,22 @@ public class CoffeeMakerTest {
 		assertEquals(null, coffeeMaker.getRecipes()[1]);
 	}
 
+	/**
+	 * Given a coffee maker 2 of same recipe it should not able to make it.
+	 */
 	@Test
 	public void testAddDuplicateRecipes2() {
 		coffeeMaker.addRecipe(recipe1);
 		assertFalse(coffeeMaker.addRecipe(recipe1));
 	}
 
+	/**
+	 * Given a coffee maker add recipe that is not valid, so it should throw the RecipeException.
+	 */
 	@Test(expected = RecipeException.class)
-	public void testUnValidRecipesException() throws RecipeException {
+	public void testInValidRecipesException() throws RecipeException {
 		recipe4 = new Recipe();
-		recipe4.setName("UnValidCoffee");
+		recipe4.setName("InValidCoffee");
 		recipe4.setAmtChocolate("-1");
 		recipe4.setAmtCoffee("-2");
 		recipe4.setAmtMilk("-2");
@@ -206,17 +263,26 @@ public class CoffeeMakerTest {
 		recipe4.setPrice("-5");
 	}
 
+	/**
+	 * Given a coffee maker 1 recipe it should able to delete it.
+	 */
 	@Test
 	public void testDeleteRecipe() {
 		coffeeMaker.addRecipe(recipe1);
 		assertEquals("Coffee", coffeeMaker.deleteRecipe(0));
 	}
 
+	/**
+	 * Given a coffee maker non recipe, it should return null if we delete recipe.
+	 */
 	@Test
 	public void testDeleteNullRecipe() {
 		assertEquals(null, coffeeMaker.deleteRecipe(0));
 	}
 
+	/**
+	 * Given a coffee maker 1 recipe, it should return recipe name when we edit or getRecipe of recipe.
+	 */
 	@Test
 	public void testEditRecipe(){
 		coffeeMaker.addRecipe(recipe1);
@@ -224,8 +290,92 @@ public class CoffeeMakerTest {
 		assertEquals("Coffee" , coffeeMaker.getRecipes()[0].getName());
 	}
 
+	/**
+	 * Given a coffee maker non recipe, it should return null if we edit recipe.
+	 */
 	@Test
 	public void testEditNoRecipe(){
 		assertEquals(null, coffeeMaker.editRecipe(0, recipe1));
+	}
+
+	/**
+	 * Add valid Inventory in part of valid sugar.
+	 * @throws InventoryException
+	 */
+	@Test
+	public void testAddSugar1() throws InventoryException{
+		coffeeMaker.addInventory("0","0","1","0");
+	}
+
+	/**
+	 * Add valid Inventory in part of invalid sugar. It will be expected that raise InventoryException.
+	 * @throws InventoryException
+	 */
+	@Test(expected = InventoryException.class)
+	public void testAddSugar2() throws InventoryException{
+		coffeeMaker.addInventory("0","0","-1","0");
+	}
+
+	/**
+	 * Add valid Inventory in part of valid coffee.
+	 * @throws InventoryException
+	 */
+	@Test
+	public void testAddCoffee1() throws InventoryException{
+		coffeeMaker.addInventory("1","0","0","0");
+	}
+
+	/**
+	 * Add valid Inventory in part of invalid coffee. It will be expected that raise InventoryException.
+	 * @throws InventoryException
+	 */
+	@Test(expected = InventoryException.class)
+	public void testAddCoffee2() throws InventoryException{
+		coffeeMaker.addInventory("-1","0","0","0");
+	}
+
+	/**
+	 * Add valid Inventory in part of valid milk.
+	 * @throws InventoryException
+	 */
+	@Test
+	public void testAddMilk1() throws InventoryException{
+		coffeeMaker.addInventory("0","1","0","0");
+	}
+
+	/**
+	 * Add valid Inventory in part of invalid milk. It will be expected that raise InventoryException.
+	 * @throws InventoryException
+	 */
+	@Test(expected = InventoryException.class)
+	public void testAddMilk2() throws InventoryException{
+		coffeeMaker.addInventory("0","-1","0","0");
+	}
+
+	/**
+	 * Add valid Inventory in part of valid chocolate.
+	 * @throws InventoryException
+	 */
+	@Test
+	public void testAddChocolate1() throws InventoryException{
+		coffeeMaker.addInventory("0","0","0","1");
+	}
+
+	/**
+	 * Add valid Inventory in part of invalid chocolate. It will be expected that raise InventoryException.
+	 * @throws InventoryException
+	 */
+	@Test(expected = InventoryException.class)
+	public void testAddChocolate2() throws InventoryException{
+		coffeeMaker.addInventory("0","0","0","-1");
+	}
+
+	/**
+	 * Add valid Inventory in part of valid value but all being 0.It should be able to do, no exception class raised.
+	 * @throws InventoryException
+	 */
+	@Test()
+	public void testAddAllZero() throws InventoryException{
+		coffeeMaker.addInventory("0","0","0","0");
 	}
 }
